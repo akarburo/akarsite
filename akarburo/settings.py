66fc21cdc0e6a77db542b254618338e7e84aea5d
akarburo <80810547+akarburo@users.noreply.github.com>
2021-03-22 10:14:08 +0300

    Update settings.py

--- akarburo/settings.py
+++ akarburo/settings.py
@@ -132,9 +132,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-EMAIL_HOST = 'mail.akarburo.com'
-EMAIL_HOST_USER = 'info@akarburo.com'
-EMAIL_HOST_PASSWORD = '11121314'
+EMAIL_HOST = '***'
+EMAIL_HOST_USER = '***'
+EMAIL_HOST_PASSWORD = '***'
 EMAIL_PORT = 25
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
